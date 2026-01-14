ca0c2a6004a1f058db6f1c9ecbc3f1edea91c301
Evans Mwaura <mwauraevans2003@gmail.com>
2026-01-14 19:07:19 +0300

    Fix Security Config public access

--- src/main/java/com/example/makaosafe/config/SecurityConfig.java
+++ src/main/java/com/example/makaosafe/config/SecurityConfig.java
@@ -30,7 +30,11 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/properties/all", "/api/properties/search").permitAll()
+                        .requestMatchers("/api/properties/add", "/api/properties/my-listings").hasRole("LANDLORD")
+                        .requestMatchers("/api/bookings/**").authenticated()
                         .requestMatchers("/api/payment/**").authenticated()
+
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
